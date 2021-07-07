c557807b15c9d3a7a9a77a5b0cbfe8852a239f1d
rkh-leung <leptanian@gmail.com>
2021-07-07 17:28:48 +1200

    fix views location path

--- server/app.js
+++ server/app.js
@@ -4,18 +4,20 @@ const PORT = 8001
 const middleware = require('./middleware')
 const path = require('path')
 const loginRoute = require('./routes/loginRoutes')
+const registerRoute = require('./routes/registerRoutes')
 
 const server = app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
 
 app.set("view engine", "pug")
-app.set("views", "views")
+app.set("views", path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/login', loginRoute)
+app.use("/login", loginRoute)
+app.use("/register", registerRoute)
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
     let payload = {
-        pageTitle: 'Home'
+        pageTitle: 'HomeSweetHome'
     }
     res.status(200).render('home', payload)
 })
