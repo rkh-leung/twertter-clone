f9b644967f026dfe5fc02ac31ec31f7a9e342e37
rkh-leung <leptanian@gmail.com>
2021-07-09 16:17:53 +1200

    enable server session

--- server/server.js
+++ server/server.js
@@ -3,16 +3,14 @@ const server = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 const result = require('dotenv').config()
+const session = require('express-session')
 
-if (result.error) {
-    throw result.error
-}
-
-// console.log(result.parsed)
+if (result.error) throw result.error
 
 server.set('view engine', 'pug')
 server.set('views', path.join(__dirname, 'views'))
 
+server.use(session({ secret: 'neovim lover', resave: false, saveUninitialized: true, cookie: {} }))
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(express.static(path.join(__dirname, 'public')))
 
@@ -22,3 +20,14 @@ server.use('/register', require('./routes/registerRoutes'))
 
 
 module.exports = server
+
+// var sess = {
+//     secret: 'keyboard cat',
+//     cookie: {}
+// }
+
+// if (app.get('env') === 'production') {
+//     app.set('trust proxy', 1) // trust first proxy
+//     sess.cookie.secure = true // serve secure cookies
+// }
+// console.log(result.parsed)
